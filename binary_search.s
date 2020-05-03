b890525ca065fda4593cf519182ce8461b173aa2
Eric Latham <ericoliverlatham@gmail.com>
2020-05-03 11:46:14 -0500

    make scan_int more robust and flexible, especially when user input is bad

--- binary_search.s
+++ binary_search.s
@@ -6,15 +6,14 @@ To run: ./binary_search
 */
 
 .data
-.n: .space 16                      # allocate memory for a scanf number variable
-.array: .quad 1, 2, 3, 4, 5        # allocate memory for an array of 5 numbers
-.search_elem: .quad 1              # allocate memory for a number to search for
+.temp: .space 16                   # allocate memory for temporary scanf number variable
+.array: .quad 1, 2, 3, 4, 5        # allocate memory for array of 5 numbers
+.search_elem: .quad 1              # allocate memory for search element
 .array_prompt: .string "Build a sorted array...\n"
 .search_prompt: .string "\nSpecify an element to search for in the array...\n"
-.search_res: .string "\nResult of Binary Search for %i: %i\n"
-.scan: .string "Enter a positive integer: "
-.format: .string "%d"
-.number: .string "%i "
+.search_result: .string "\nResult of Binary Search for %i: %i\n"
+.scan: .string "Enter an integer: "
+.format: .string "%d"              # format string for scanning and printing integers
 
 .text
 .global main
@@ -38,7 +37,7 @@ main:
     movq .search_elem, %rcx
     call binary_search             # search for the element using binary search
 
-    movq $.search_res, %rdi
+    movq $.search_result, %rdi
     movq .search_elem, %rsi
     movq %rax, %rdx
     call print                     # print the result of search
@@ -64,7 +63,7 @@ take_input:
 .inside_input_loop:
     movq $.scan, %rdi
     call print
-    call scan
+    call scan_int
     movq %rax, (%rbx, %r10, 8)     # insert scanned value into destination
     inc %r10
     jmp .input_loop
@@ -120,7 +119,7 @@ print:
     push %r12
     push %r13
     push %r14
-    xor %rax, %rax
+    xorq %rax, %rax
     call printf
     pop %r14
     pop %r13
@@ -133,22 +132,51 @@ print:
     ret
 
 /*
+To clear the stdin input buffer after a failed scanf call, read input
+from stdin until '\n' or EOF are reached, returning control to caller
+if '\n' is reached and exiting the program if EOF is reached.
+*/
+clear_input_buffer:
+.read_loop:
+    call getchar                   # %eax = char (4-byte int)
+    cmp $10, %eax                  # see if %eax == '\n'
+    je .newline_case               # jump to .newline_case if %eax == '\n'
+    test %eax, %eax                # set flags according to %eax
+    js .eof_case                   # jump to .eof_case if %eax is negative
+    jmp .read_loop                 # continue loop
+.newline_case:
+    ret
+.eof_case:
+    movq $0, %rdi
+    call exit                      # exit(0)
+
+/*
 Use scanf to safely scan for an integer value from stdin, passing .format
-as the format string and .n for temporary destination storage.
+as the format string and .temp for temporary destination storage.
 Push and pop registers to preserve their values and use
 a stack canary for added system compatibility.
 */
-scan:
+scan_int:
     push %rbx
     push %rcx
     push %r10
     subq $64, %rsp                 # stack canary
-	movq $.format, %rdi            # param 1, format string
-	xorq %rax, %rax                # empty %rax
-	leaq .n(%rip), %rsi            # param 2, scan destination
-	call __isoc99_scanf@PLT        # scanf function call
-	addq $64, %rsp                 # stack canary reset
-    movq .n(%rip), %rax            # move scanned value to %rax for return
+	movq $.format, %rdi            # set param 1: format string
+	leaq .temp(%rip), %rsi         # set param 2: scan destination
+    xorq %rax, %rax                # empty %rax
+	call __isoc99_scanf@PLT        # call scanf
+    addq $64, %rsp                 # reset stack canary
+    cmpq $1, %rax                  # see if scanf succeeded
+    je .success                    # jump to .success if scanf succeeded
+    jmp .default                   # jump to .default if scanf failed
+.success:
+    movq .temp(%rip), %rax         # return scanned value
+    jmp .return
+.default:
+    call clear_input_buffer        # clear input buffer for next scanf call
+    movq $0, %rax                  # return default value
+    jmp .return
+.return:
     pop %r10
     pop %rcx
     pop %rbx
